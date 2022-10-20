6585a4604d44236ee1a5cc6d3401956609564261
RaviAnand Mohabir <moravrav@gmail.com>
2022-10-20 17:24:24 +0200

    feat: :sparkles: add name entry modal and deny access if name isn't set

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,75 @@
+import {
+  Box,
+  Button,
+  Container,
+  Modal,
+  Stack,
+  TextInput,
+  Title,
+  useMantineTheme,
+} from "@mantine/core";
+import { useLocalStorage } from "@mantine/hooks";
+import { IconArrowBigRightLine, IconHash, IconUser } from "@tabler/icons";
 import type { NextPage } from "next";
-import { Box, Button, Container, Input, Title, Stack } from "@mantine/core";
-import { IconArrowBigRightLine, IconAt, IconHash } from "@tabler/icons";
+import { useRouter } from "next/router";
+import { useCallback, useState } from "react";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  const theme = useMantineTheme();
+
+  const [modalOpened, setModalOpened] = useState(false);
+  const [nextRoute, setNextRoute] = useState("");
+
+  const [nameInput, setNameInput] = useState("");
+  const [nameInputValid, setNameInputValid] = useState(true);
+
+  const [partyCode, setPartyCode] = useState("");
+  const [partyCodeValid, setPartyCodeValid] = useState(true);
+
+  const [username, setUsername] = useLocalStorage<string>({
+    key: "jamboree-username",
+    getInitialValueInEffect: true,
+  });
+
+  const createParty = useCallback(() => {
+    if (!username) {
+      setModalOpened(true);
+      setNextRoute("/parties/create");
+    }
+    router.push("/parties/create");
+  }, [username, router]);
+
+  const joinParty = useCallback(
+    (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      if (!partyCode) {
+        setPartyCodeValid(false);
+      } else if (!username) {
+        setModalOpened(true);
+        setNextRoute(`/parties/join/${partyCode}`);
+      } else {
+        router.push(`/parties/join/${partyCode}`);
+      }
+    },
+    [username, router, partyCode]
+  );
+
+  const onModalClose = useCallback(() => {}, []);
+
+  const onNameSubmit = useCallback(
+    (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      if (nameInput) {
+        setUsername(nameInput);
+        router.push(nextRoute);
+      } else {
+        setNameInputValid(false);
+      }
+    },
+    [setUsername, nameInput, nextRoute, router, setNameInputValid]
+  );
+
   return (
     <Container
       sx={{
@@ -12,6 +79,38 @@ const Home: NextPage = () => {
         gap: 64,
       }}
     >
+      <Modal
+        opened={modalOpened}
+        onClose={onModalClose}
+        title="Introduce yourself!"
+        centered
+        overlayColor={
+          theme.colorScheme === "dark"
+            ? theme.colors.dark[9]
+            : theme.colors.gray[2]
+        }
+        overlayOpacity={0.55}
+        overlayBlur={3}
+      >
+        <form onSubmit={onNameSubmit}>
+          <Stack>
+            <TextInput
+              label="Enter your Name"
+              icon={<IconUser />}
+              onChange={(e) => setNameInput(e.target.value)}
+              error={!nameInputValid}
+            />
+            <Button
+              sx={{ marginLeft: "auto" }}
+              color="pink"
+              variant="outline"
+              type="submit"
+            >
+              Go!
+            </Button>
+          </Stack>
+        </form>
+      </Modal>
       <Box sx={{ height: 64 }} />
       <Title
         sx={{ fontFamily: "Lobster", fontSize: "4rem", textAlign: "center" }}
@@ -25,13 +124,22 @@ const Home: NextPage = () => {
         color="pink"
         radius="lg"
         size="md"
+        onClick={createParty}
       >
         Plan Now
       </Button>
-      <Stack>
-        <Title>Have a Party Code?</Title>
-        <Input icon={<IconHash />} placeholder="your-party-code" size="md" />
-      </Stack>
+      <form onSubmit={joinParty}>
+        <Stack>
+          <Title>Have a Party Code?</Title>
+          <TextInput
+            icon={<IconHash />}
+            placeholder="your-party-code"
+            size="md"
+            error={!partyCodeValid}
+            onChange={(e) => setPartyCode(e.target.value)}
+          />
+        </Stack>
+      </form>
     </Container>
   );
 };
