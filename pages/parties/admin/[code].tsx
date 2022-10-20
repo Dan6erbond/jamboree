79effaf7ba36014d6ce4905b4a3a644cc0e480ba
RaviAnand Mohabir <moravrav@gmail.com>
2022-10-20 23:06:10 +0200

    feat: :sparkles: implement data fetching and ui improvements to admin view

--- pages/parties/admin/[code].tsx
+++ pages/parties/admin/[code].tsx
@@ -17,14 +17,30 @@ import {
 import { DatePicker, TimeInput } from "@mantine/dates";
 import { useClipboard } from "@mantine/hooks";
 import { IconCopy, IconHash, IconMapPin, IconPlus } from "@tabler/icons";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { NextPage } from "next";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { firestore } from "../../../src/firebase/firestore";
 import { useRandomName } from "../../../src/hooks/useRandomName";
 
 const Admin: NextPage = () => {
+  const router = useRouter();
   const { randomName } = useRandomName();
 
   const clipboard = useClipboard({ timeout: 500 });
+
+  useEffect(() => {
+    (async () => {
+      const partiesRef = collection(firestore, "parties");
+      const q = query(partiesRef, where("adminCode", "==", router.query.code));
+      const snapshots = await getDocs(q);
+      snapshots.forEach((snapshot) => {
+        console.log(snapshot.id, snapshot.data());
+      });
+    })();
+  });
 
   return (
     <Container>
@@ -33,14 +49,11 @@ const Admin: NextPage = () => {
           Admin
         </Title>
         <Group>
-          <ActionIcon
-            color="pink"
-            variant="light"
-            sx={{ padding: 4 }}
-            onClick={() => clipboard.copy(randomName)}
-          >
-            <IconHash />
-          </ActionIcon>
+          <Tooltip label="Copy URL">
+            <ActionIcon color="pink" variant="light" sx={{ padding: 4 }}>
+              <IconHash />
+            </ActionIcon>
+          </Tooltip>
           <Title
             order={2}
             size="h1"
@@ -122,27 +135,34 @@ const Admin: NextPage = () => {
                   : theme.colors.gray[2],
             })}
           >
-            <Title>Admin Zone</Title>
-            <Text>
-              This is your admin page, make sure you save the link or copy it so
-              you can use it later to edit your party. You can also share this
-              link with others.
-            </Text>
-            <Group position="center">
-              <ActionIcon
-                color="pink"
-                variant="light"
-                sx={{ padding: 4 }}
-                onClick={() => clipboard.copy(window.location.href)}
-              >
-                <IconCopy />
-              </ActionIcon>
-              <Link href={window.location.href} passHref>
-                <Text component="a" color="dimmed">
-                  {window.location.href}
-                </Text>
-              </Link>
-            </Group>
+            <Stack>
+              <Title>Admin Zone</Title>
+              <Text>
+                This is your admin page, make sure you save the link or copy it
+                so you can use it later to edit your party. You can also share
+                this link with others.
+              </Text>
+              {/* TODO: Make client-side only and add hostname with window.location */}
+              <Group position="center">
+                <ActionIcon
+                  color="pink"
+                  variant="light"
+                  sx={{ padding: 4 }}
+                  onClick={() => clipboard.copy(router.asPath)}
+                >
+                  <IconCopy />
+                </ActionIcon>
+                <Link href={router.asPath} passHref>
+                  <Text
+                    component="a"
+                    color="dimmed"
+                    sx={{ wordBreak: "break-all" }}
+                  >
+                    {router.asPath}
+                  </Text>
+                </Link>
+              </Group>
+            </Stack>
           </Card>
           <Button
             color="pink"
