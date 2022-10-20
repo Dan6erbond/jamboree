4f468109753cb2d2ba1f3dbf028dc4959f4a888c
RaviAnand Mohabir <moravrav@gmail.com>
2022-10-20 15:42:50 +0200

    feat: :heavy_plus_sign: add mantine with Next.js cookie-based theme config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,88 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@fontsource/lobster";
+import {
+  AppShell,
+  ColorScheme,
+  ColorSchemeProvider,
+  Navbar,
+  Title,
+  MantineProvider,
+  Header,
+} from "@mantine/core";
+import { useColorScheme, useHotkeys, useLocalStorage } from "@mantine/hooks";
+import { getCookie, setCookie } from "cookies-next";
+import { GetServerSidePropsContext } from "next";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const preferredColorScheme = useColorScheme();
+
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: "mantine-color-scheme",
+    defaultValue: preferredColorScheme,
+    getInitialValueInEffect: true,
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) => {
+    const nextColorScheme =
+      value || (colorScheme === "dark" ? "light" : "dark");
+    setColorScheme(nextColorScheme);
+    // when color scheme is updated save it to cookie
+    setCookie("mantine-color-scheme", nextColorScheme, {
+      maxAge: 60 * 60 * 24 * 30,
+    });
+  };
+
+  useHotkeys([["mod+J", () => toggleColorScheme()]]);
+
+  return (
+    <>
+      <Head>
+        <title>Jamboree</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            colorScheme,
+          }}
+        >
+          <AppShell
+            padding="md"
+            header={
+              <Header height={70} p="md">
+                <Title sx={{ fontFamily: "Lobster" }}>Jamboree 🎉</Title>
+              </Header>
+            }
+            sx={(theme) => ({
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[6]
+                  : theme.colors.gray[0],
+              height: "100vh",
+              width: "100vw",
+            })}
+          >
+            <Component {...pageProps} />
+          </AppShell>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+MyApp.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
+  // get color scheme from cookie
+  colorScheme: getCookie("mantine-color-scheme", ctx) || "light",
+});
+
+export default MyApp;
