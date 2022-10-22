d67d6d6f5c7364e089a4e28321519cceac6ce9f1
RaviAnand Mohabir <moravrav@gmail.com>
2022-10-23 00:25:44 +0200

    feat: :sparkles: remove firebase and other dependencies
    Using an actual backend now hehe.

--- src/firebase/analytics.ts
+++
@@ -1,4 +0,0 @@
-import { getAnalytics } from "firebase/analytics";
-import { app } from ".";
-
-export const analytics = getAnalytics(app);
--- src/types/location.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "./user";
-import { UUID } from "./uuid";
-
-export interface Location {
-  location: string;
-  uuid: UUID;
-  votes: User[];
-}
