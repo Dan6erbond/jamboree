17858f5412ee699a5c83fbb37e60d3513969bf31
RaviAnand Mohabir <moravrav@gmail.com>
2022-10-24 10:55:11 +0000

    feat: :sparkles: use separate env vars for backend and frontend apollo config

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
   // TODO: Configure client for production
-  uri: `http://${process.env.NEXT_PUBLIC_JAMBOREE_API_HOST}/${process.env.NEXT_PUBLIC_JAMBOREE_API_PATH}`,
+  uri: process.env.NEXT_PUBLIC_JAMBOREE_API_URL,
   cache: new InMemoryCache({
     typePolicies: {
       Party: {
