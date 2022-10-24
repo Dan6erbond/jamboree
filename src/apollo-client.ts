93aa07527d042b9f037c7d0727cbee257669bcb3
RaviAnand Mohabir <moravrav@gmail.com>
2022-10-24 10:34:24 +0000

    feat: :sparkles: use environment variables for apollo client config

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
   // TODO: Configure client for production
-  uri: "http://localhost:5001/graphql",
+  uri: `http://${process.env.NEXT_PUBLIC_JAMBOREE_API_HOST}/${process.env.NEXT_PUBLIC_JAMBOREE_API_PATH}`,
   cache: new InMemoryCache({
     typePolicies: {
       Party: {
